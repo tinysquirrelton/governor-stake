baff4a39ef1cb60795c90b94840203fcc6ecd1b1
tinysquirrelton <soheil.saadati@hotmail.com>
2021-01-25 22:39:49 +0100

    nft bones

--- src/components/stake/index.js
+++ src/components/stake/index.js
@@ -47,51 +47,58 @@ export default class Stake extends Component {
           <div className="nft-subtitle">Coming Soon</div>
 
           <div className="nft-card-container">
-            <div id="cards">
-              <figure class="card card--dark">
-                <div class="card__image-container">
-                  <img
-                    src="https://media.giphy.com/media/26BRqPg05olzXG1bi/giphy.gif"
-                    alt="Eevee"
-                    class="card__image"
-                  />
-                </div>
-                <figcaption class="card__caption">
-                  <h1 class="card__name">XXXXXXX</h1>
-                  <h3 class="card__type">Coming soon</h3>
-                  <table class="card__stats">
-                    <tbody>
-                      <tr>
-                        <th>XXXXXXX</th>
-                        <td>YYYYYYY</td>
-                      </tr>
-                      <tr>
-                        <th>XXXXXXX</th>
-                        <td>YYYYYYY</td>
-                      </tr>
-                      <tr>
-                        <th>XXXXXXX</th>
-                        <td>YYYYYYY</td>
-                      </tr>
-                      <tr>
-                        <th>XXXXXXX</th>
-                        <td>YYYYYYY</td>
-                      </tr>
-                    </tbody>
-                  </table>
-                  <div class="card__abilities">
-                    <h4 class="card__ability">
-                      <span class="card__label">XXXXXXX</span>
-                      YYYYYYY
-                    </h4>
-                    <h4 class="card__ability">
-                      <span class="card__label">XXXXXXX</span>
-                      YYYYYYY
-                    </h4>
-                  </div>
-                </figcaption>
-              </figure>
-            </div>
+            <figure class="card card--dark">
+              <div class="card__image-container">
+                <img
+                  src="https://media.giphy.com/media/26BRqPg05olzXG1bi/giphy.gif"
+                  alt="Eevee"
+                  class="card__image"
+                />
+              </div>
+              <figcaption class="card__caption">
+                <h1 class="card__name">?</h1>
+                <h3 class="card__type">Coming soon</h3>
+              </figcaption>
+            </figure>
+            <figure class="card card--dark">
+              <div class="card__image-container">
+                <img
+                  src="https://media.giphy.com/media/26BRqPg05olzXG1bi/giphy.gif"
+                  alt="Eevee"
+                  class="card__image"
+                />
+              </div>
+              <figcaption class="card__caption">
+                <h1 class="card__name">?</h1>
+                <h3 class="card__type">Coming soon</h3>
+              </figcaption>
+            </figure>
+            <figure class="card card--dark">
+              <div class="card__image-container">
+                <img
+                  src="https://media.giphy.com/media/26BRqPg05olzXG1bi/giphy.gif"
+                  alt="Eevee"
+                  class="card__image"
+                />
+              </div>
+              <figcaption class="card__caption">
+                <h1 class="card__name">?</h1>
+                <h3 class="card__type">Coming soon</h3>
+              </figcaption>
+            </figure>
+            <figure class="card card--dark">
+              <div class="card__image-container">
+                <img
+                  src="https://media.giphy.com/media/26BRqPg05olzXG1bi/giphy.gif"
+                  alt="Eevee"
+                  class="card__image"
+                />
+              </div>
+              <figcaption class="card__caption">
+                <h1 class="card__name">?</h1>
+                <h3 class="card__type">Coming soon</h3>
+              </figcaption>
+            </figure>
           </div>
         </div>
         <div className="gdao-texture-bg" />
