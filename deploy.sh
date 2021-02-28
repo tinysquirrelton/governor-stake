78a03dc90b7ea6354c734fe804dc3a91d3ef621d
tinysquirrelton <soheil.saadati@hotmail.com>
2021-02-28 22:54:52 +0100

    downtime fix

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 npm install
 npm uninstall governor-common
 npm install https://github.com/tinysquirrelton/governor-common.git
-npm run build
+BUILD_PATH=temp_build npm run build
+rm -rf build
+mv temp_build build
